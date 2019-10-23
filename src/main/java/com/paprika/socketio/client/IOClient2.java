4e507b55b812564be778dd5ed44ec5a51cdab489
Leiothrix <Leiothrix@users.noreply.github.com>
2019-10-23 18:38:58 +0800

    Initial commit

---
+++ src/main/java/com/paprika/socketio/client/IOClient2.java
@@ -0,0 +1,29 @@
+package com.paprika.socketio.client;
+
+import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * @author adam
+ * @date 2019/10/23
+ */
+@Slf4j
+public class IOClient2 {
+    public static void main(String[] args) {
+        new Thread(() -> {
+            try {
+                Socket socket = new Socket("127.0.0.1", 8000);
+                while (true) {
+                    try {
+                        socket.getOutputStream().write(("Hi adam!").getBytes());
+                        socket.getOutputStream().flush();
+                        Thread.sleep(2000);
+                    } catch (Exception e) {
+                    }
+                }
+            } catch (IOException e) {
+            }
+        }).start();
+    }
+}
